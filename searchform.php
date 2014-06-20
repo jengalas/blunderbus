d071acbccf00b9387c2acda0538a18a47be895ca
Jennifer Galas <jennifer.galas@scranton.edu>
2014-06-20 18:12:06 -0400

    Added .form-inline

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
  * @package Surveymarks
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="form-inline search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'surveymarks' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'surveymarks' ); ?>">
 	</label>
