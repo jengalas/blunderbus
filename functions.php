36e5d9a1ed1b66cba8923638f2156e4f7377a158
Jennifer Galas <jennifer.galas@scranton.edu>
2014-06-24 21:44:33 -0400

    Renamed primary widget area

--- functions.php
+++ functions.php
@@ -78,8 +78,16 @@ add_action( 'after_setup_theme', 'blunderbus_setup' );
  */
 function blunderbus_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'blunderbus' ),
+		'name'          => __( 'Primary Widget Area', 'blunderbus' ),
 		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Secondary Widget Area', 'blunderbus' ),
+		'id'            => 'sidebar-2',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h3 class="widget-title">',
