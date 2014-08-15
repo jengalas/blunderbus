50899913c5cb8653b0cc9d2d17bda9dfec9f9437
Jennifer Galas <jennifer.galas@scranton.edu>
2014-08-15 08:03:42 -0400

    Change search name so it works :)

--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@
 ?>
 <form role="search" method="get" class="form-inline search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group">
-            <input type="text" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Keyword search &hellip;', 'placeholder', 'blunderbus' ); ?>" name="srch-term" id="srch-term">
+            <input type="text" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Keyword search &hellip;', 'placeholder', 'blunderbus' ); ?>" name="s" id="srch-term">
             <div class="input-group-btn">
                 <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
             </div>
