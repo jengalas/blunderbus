47ea12984673cdfa76783d552ce69b2f96f9b7a4
Jennifer Galas <jennifer.galas@scranton.edu>
2014-09-01 20:32:37 -0400

    Add button and form control styles

--- includes/js/bootstrap-wp.js
+++ includes/js/bootstrap-wp.js
@@ -8,8 +8,12 @@ jQuery( document ).ready( function( $ ) {
     // here for each comment reply link of wordpress
     $( '.comment-reply-link' ).addClass( 'btn btn-primary' );
 
-    // here for the submit button of the comment reply form
-    $( '#commentsubmit' ).addClass( 'btn btn-primary' );
+    // here for the submit button of the comment reply form and contact form
+    $( '#commentsubmit, div.frm_submit input' ).addClass( 'btn btn-primary' );
+
+    // some more styles for comment form and Formidable contact form. See main styles in wp-content/plugins/formidable/css/frm_display.css
+
+    $( 'div.frm_form_field input, div.frm_form_field textarea, form.wpv-filter-form input, form.wpv-filter-form select, #commentform input[type="text"]' ).addClass( 'form-control' );
 
     // The WordPress Default Widgets
     // Now we'll add some classes for the wordpress default widgets - let's go
